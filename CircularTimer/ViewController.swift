3abe36e2269e5392d226cc9ecbaa335ea9e88c9e
Shamil Yusuf <shamil707@gmail.com>
2018-10-07 14:18:05 +0800

    Added the Timer Functionality

--- CircularTimer/ViewController.swift
+++ CircularTimer/ViewController.swift
@@ -5,21 +5,83 @@
 //  Created by Shamil Yusuf on 7/10/18.
 //  Copyright © 2018 MiteSolution. All rights reserved.
 //
-
 import UIKit
-
 class ViewController: UIViewController {
-
+    let timeLeftShapeLayer = CAShapeLayer()
+    let bgShapeLayer = CAShapeLayer()
+    var timeLeft: TimeInterval = 10
+    var endTime: Date?
+    var timeLabel =  UILabel()
+    var timer = Timer()
+    // here you create your basic animation object to animate the strokeEnd
+    let strokeIt = CABasicAnimation(keyPath: "strokeEnd")
+    fileprivate func startRunning() {
+        strokeIt.fromValue = 0
+        strokeIt.toValue = 1
+        strokeIt.duration = timeLeft
+        timeLeftShapeLayer.add(strokeIt, forKey: nil)
+        endTime = Date().addingTimeInterval(timeLeft)
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor(white: 0.94, alpha: 1.0)
+        drawBgShape()
+        drawTimeLeftShape()
+        addTimeLabel()
     }
-
+    @IBAction func resetButtonClick(_ sender: Any) {
+        timer.invalidate()
+        timeLabel.text = "00:00"
+        timeLeft = 30
+        startRunning()
+    }
+    @IBAction func startButtonClick(_ sender: Any) {
+        startRunning()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func drawBgShape() {
+        bgShapeLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX , y: view.frame.midY), radius:
+            100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+        bgShapeLayer.strokeColor = UIColor.white.cgColor
+        bgShapeLayer.fillColor = UIColor.clear.cgColor
+        bgShapeLayer.lineWidth = 15
+        view.layer.addSublayer(bgShapeLayer)
+    }
+    func drawTimeLeftShape() {
+        timeLeftShapeLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX , y: view.frame.midY), radius:
+            100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+        timeLeftShapeLayer.strokeColor = UIColor.red.cgColor
+        timeLeftShapeLayer.fillColor = UIColor.clear.cgColor
+        timeLeftShapeLayer.lineWidth = 15
+        view.layer.addSublayer(timeLeftShapeLayer)
+    }
+    func addTimeLabel() {
+        timeLabel = UILabel(frame: CGRect(x: view.frame.midX-50 ,y: view.frame.midY-25, width: 100, height: 50))
+        timeLabel.textAlignment = .center
+        timeLabel.text = timeLeft.time
+        view.addSubview(timeLabel)
+    }
+    @objc func updateTime() {
+        if timeLeft > 0 {
+            timeLeft = endTime?.timeIntervalSinceNow ?? 0
+            timeLabel.text = timeLeft.time
+        } else {
+            timeLabel.text = "00:00"
+            timer.invalidate()
+        }
+    }
 }
-
+extension TimeInterval {
+    var time: String {
+        return String(format:"%02d:%02d", Int(self/60),  Int(ceil(truncatingRemainder(dividingBy: 60))) )
+    }
+}
+extension Int {
+    var degreesToRadians : CGFloat {
+        return CGFloat(self) * .pi / 180
+    }
+}
